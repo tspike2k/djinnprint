004f58a842b7de37916bdca2f0198bd75c9f911c
tspike <mail@gamedevgp.net>
2020-08-27 04:41:29 -0400

    Improved safeCopy to flag when truncation occurs.

--- djinnprint.d
+++ djinnprint.d
@@ -13,33 +13,46 @@
 
 // - Custom float/double to string conversion that doesn't rely on snprintf
 
+// - Put quotes around string values when printing functions/unions
+
+// - Figure out how to reduce code duplication between the two versions of formatArg
+
 // - Add formatting options for variables (commas for integers, hex output, etc.)
 //   Additionally, there should be an option to print the name of each struct type before the value of its members. This could be useful in code generation.
 //   For instance, printing `Vect2(1.0000f, 1.0000f)` would be useful for this case rather than `(1.0000, 1.000)`, the latter of which is the default behavior.
 
+// NOTE: The order of members returned by __traits(allMembers) is not guaranteed to be in the orde they appear in the struct definiation.
+// However, it SEEMS that the .tupleof property is expected (perhaps even required) to be ordered this way. This behavior is what we're relying on.
+// Should it change, we're going to have to make some changes.
+//
+// See here for some discussions on this topic:
+// https://forum.dlang.org/thread/bug-19036-3@https.issues.dlang.org%2F
+// https://forum.dlang.org/thread/odpdhayvxaglheqcntwj@forum.dlang.org
+// https://forum.dlang.org/post/stvphdwgugrlcgfkbyxc@forum.dlang.org
+
 module djinnprint;
 
-private:
+enum ToPrint;
 
-import std.traits;
-
-// NOTE: Uncomment the lines below to alter the behavior of the library.
-//version = assertOnTruncation; // Trigger an assertion when formatting to a buffer results in a string larger than the buffer size
-//version = assertOnUnhandledUnion; // Assert when a union without ToPrint or ToPrintWhen UDAs is found while formatting.
-
-public enum ToPrint;
-
-public struct ToPrintWhen(T)
+struct ToPrintWhen(T)
 {
     string unionTag;
     T[]      cases;
     string[] members;
 }
 
-@nogc nothrow:
+private @nogc nothrow:
 
-// Flags to configure the library
-enum use_cstdio = true;
+// Library configuration options:
+enum assertOnTruncation     = false; // Trigger an assertion when formatting to a buffer results in a string larger than the buffer size
+enum assertOnUnhandledUnion = false; // Assert when a union without ToPrint or ToPrintWhen UDAs is found while formatting.
+enum use_cstdio             = true; // Use the standard C library output functions when printing to the console
+
+import std.traits;
+
+alias ArrayTarget(T : U[], U) = U;
+enum bool isCString(T) = is(T == char*) || is(T == const(char)*) || is(T == immutable(char)*);
+enum bool isCharArray(T) = isArray!T && (is(ArrayTarget!(T) == char) || is(ArrayTarget!(T) == immutable char) || is(ArrayTarget!(T) == const char));
 
 static if(use_cstdio)
 {
@@ -47,6 +60,8 @@ static if(use_cstdio)
     alias FileHandle = FILE*;
     alias stdOut = stdout;
     alias stdErr = stderr;
+
+    public void init(){};
 }
 else
 {
@@ -103,6 +118,7 @@ size_t length(const(char*) s)
 ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
 {
     ulong bytesWritten = 0;
+    bool truncated = false;
 
     static if (is(T == enum))
     {
@@ -110,7 +126,7 @@ ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
         {
             if (t == member)
             {
-                bytesWritten = safeCopy(buffer, __traits(identifier, EnumMembers!T[i]));
+                bytesWritten = safeCopy(buffer, __traits(identifier, EnumMembers!T[i]), &truncated);
             }
         }
     }
@@ -118,11 +134,11 @@ ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
     {
         if(t)
         {
-            bytesWritten = safeCopy(buffer, "true");
+            bytesWritten = safeCopy(buffer, "true", &truncated);
         }
         else
         {
-            bytesWritten = safeCopy(buffer, "false");
+            bytesWritten = safeCopy(buffer, "false", &truncated);
         }
     }
     else static if (isIntegral!T)
@@ -136,78 +152,43 @@ ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
         import core.stdc.stdio : snprintf;
         bytesWritten = snprintf(buffer.ptr, buffer.length, "%f", t);
     }
-    else static if(is(T == char*) || is(T == const(char)*) || is(T == immutable(char)*))
+    else static if(isCString!T)
     {
-        version(assertOnTruncation)
-        {
-            bytesWritten = size_t.max;
-        }
-        else
-        {
-            bytesWritten = buffer.length - 1;
-        }
+        size_t srcLength = 0;
+        while(t[srcLength] != '\0') srcLength++;
 
-        foreach(charIndex; 0 .. buffer.length - 1)
-        {
-            buffer[charIndex] = t[charIndex];
-            if(t[charIndex] == '\0')
-            {
-                bytesWritten = charIndex;
-                break;
-            }
-        }
-
-        version(assertOnTruncation)
-        {
-            assert(bytesWritten != size_t.max);
-        }
+        bytesWritten = safeCopy(buffer, t[0 .. srcLength], &truncated);
     }
-    else static if((isArray!T && is(typeof(t[0]) == char)) || is(T == string))
+    else static if(isCharArray!T)
     {
-        if (buffer.length < t.length)
-        {
-            bytesWritten = buffer.length;
-            version(assertOnTruncation) assert(0, "ERR: buffer truncated.");
-        }
-        else
-        {
-            bytesWritten = t.length;
-        }
-
-        buffer[0..bytesWritten] = t[0..bytesWritten];
+        bytesWritten = safeCopy(buffer, t, &truncated);
     }
     else static if(is(T == struct))
     {
-        // TODO: Test how this behaves when it runs out of room.
-        // TODO: Assert on truncation
-        bytesWritten += safeCopy(buffer, "(");
-        alias allMembers = __traits(allMembers, T);
-        static foreach(i, memberString; allMembers)
+        bytesWritten += safeCopy(buffer[bytesWritten .. $], "(", &truncated);
+        auto members = t.tupleof;
+        static foreach(i, member; members)
         {
-            bytesWritten += formatArg(mixin("t." ~ memberString), spec, buffer[bytesWritten .. $]);
-            static if(i < allMembers.length - 1)
-            {
-                bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ");
-            }
+            bytesWritten += formatArg(member, spec, buffer[bytesWritten .. $]);
+            static if(i < members.length - 1) bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ", &truncated);
         }
-
-        bytesWritten += safeCopy(buffer[bytesWritten .. $], ")");
+        bytesWritten += safeCopy(buffer[bytesWritten .. $], ")", &truncated);
     }
     else static if(is(T == union))
     {
         alias toPrintMembers = getSymbolsByUDA!(T, ToPrint);
         static if (toPrintMembers.length > 0)
         {
-            bytesWritten += safeCopy(buffer[bytesWritten .. $], "(");
+            bytesWritten += safeCopy(buffer[bytesWritten .. $], "(", &truncated);
             static foreach(i, member; toPrintMembers)
             {
                 bytesWritten += formatArg(mixin("t." ~ member.stringof), spec, buffer[bytesWritten .. $]);
                 static if (i < toPrintMembers.length - 1)
                 {
-                    bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ");
+                    bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ", &truncated);
                 }
             }
-            bytesWritten += safeCopy(buffer[bytesWritten .. $], ")");
+            bytesWritten += safeCopy(buffer[bytesWritten .. $], ")", &truncated);
         }
         else if (hasUDA!(T, ToPrintWhen))
         {
@@ -227,30 +208,30 @@ ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
         }
         else
         {
-            version(assertOnUnhandledUnion)
+            static if(assertOnUnhandledUnion)
             {
                 pragma(msg, "ERR: Unhandled union " ~ T.stringof ~ ". No @toPrint UDA found.");
                 static assert(0);
             }
 
-            bytesWritten += safeCopy(buffer[bytesWritten .. $], "union ");
-            bytesWritten += safeCopy(buffer[bytesWritten .. $], T.stringof);
+            bytesWritten += safeCopy(buffer[bytesWritten .. $], "union ", &truncated);
+            bytesWritten += safeCopy(buffer[bytesWritten .. $], T.stringof, &truncated);
         }
     }
     else static if(isArray!T)
     {
-        bytesWritten += safeCopy(buffer[bytesWritten .. $], "[");
+        bytesWritten += safeCopy(buffer[bytesWritten .. $], "[", &truncated);
 
         foreach(i; 0 .. t.length)
         {
             bytesWritten += formatArg(t[i], spec, buffer[bytesWritten .. $]);
             if (i < t.length - 1)
             {
-                bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ");
+                bytesWritten += safeCopy(buffer[bytesWritten .. $], ", ", &truncated);
             }
         }
 
-        bytesWritten += safeCopy(buffer[bytesWritten .. $], "]");
+        bytesWritten += safeCopy(buffer[bytesWritten .. $], "]", &truncated);
     }
     else static if (isPointer!T)
     {
@@ -261,6 +242,8 @@ ulong formatArg(T)(T t, in FormatSpec spec, char[] buffer)
         pragma(msg, "ERR in print.formatArg(...): Unhandled type " ~ T.stringof);
         static assert(0);
     }
+
+    static if(assertOnTruncation) assert(!truncated);
 
     return bytesWritten;
 }
@@ -303,26 +286,23 @@ void formatArg(T)(T t, in FormatSpec spec, FileHandle file)
         auto written = snprintf(buffer.ptr, buffer.length, "%f", t);
         printFile(file, buffer[0..written]);
     }
-    else static if(is(T == char*) || is(T == const(char)*) || is(T == immutable(char)*))
+    else static if(isCString!T)
     {
         auto msg = t[0 .. length(t)];
         printFile(file, msg);
     }
-    else static if((isArray!T && is(typeof(t[0]) == char)) || is(T == string))
+    else static if(isCharArray!T)
     {
         printFile(file, t);
     }
     else static if(is(T == struct))
     {
         printFile(file, "(");
-        alias allMembers = __traits(allMembers, T);
-        static foreach(i, memberString; allMembers)
+        auto members = t.tupleof;
+        static foreach(i, member; members)
         {
-            formatArg(mixin("t." ~ memberString), spec, file);
-            static if (i < allMembers.length - 1)
-            {
-                printFile(file, ", ");
-            }
+            formatArg(member, spec, file);
+            static if(i < members.length - 1) printFile(file, ", ");
         }
         printFile(file, ")");
     }
@@ -360,7 +340,7 @@ void formatArg(T)(T t, in FormatSpec spec, FileHandle file)
         }
         else
         {
-            version(assertOnUnhandledUnion)
+            static if(assertOnUnhandledUnion)
             {
                 pragma(msg, "ERR: Unhandled union " ~ T.stringof ~ ". No @toPrint UDA found.");
                 static assert(0);
@@ -436,10 +416,21 @@ FormatSpec getFormatSpec(in char[] command)
     return result;
 }
 
-size_t safeCopy(T)(char[] dest, T source)
-if(isSomeString!T || isSomeChar!(source([0])))
+size_t safeCopy(T)(char[] dest, T source, bool* truncated)
+if (isCharArray!T)
 {
-    size_t bytesToCopy = dest.length < source.length ? dest.length : source.length;
+    size_t bytesToCopy = void;
+
+    if(dest.length < source.length)
+    {
+        bytesToCopy = dest.length;
+        *truncated = true;
+    }
+    else
+    {
+        bytesToCopy = source.length;
+    }
+
     dest[0 .. bytesToCopy] = source[0 .. bytesToCopy];
     return bytesToCopy;
 }
@@ -494,7 +485,7 @@ if(isIntegral!T)
     }
     else
     {
-        version(assertOnTruncation) assert(0, "ERR: buffer truncated.");
+        static if(assertOnTruncation) assert(0, "ERR: buffer truncated.");
         minToCopy = buffer.length;
     }
 
@@ -571,21 +562,26 @@ enum printCommon = `
 `;
 
 char[] format(T, Args...)(T fmt, char[] buffer, Args args)
-if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
+if(isCharArray!T)
 {
     size_t bufferWritten = 0;
-    enum outputPolicy = `bufferWritten += safeCopy(buffer[bufferWritten..buffer.length], fmt[fmtCopyToBufferIndex .. fmtCursor]);`;
+    bool truncated = false;
+
+    enum outputPolicy = `bufferWritten += safeCopy(buffer[bufferWritten..buffer.length], fmt[fmtCopyToBufferIndex .. fmtCursor], &truncated);`;
     enum formatPolicy = `bufferWritten += formatArg(args[i], formatSpec, buffer[bufferWritten .. buffer.length]);`;
 
     mixin(printCommon);
 
     size_t zeroIndex = bufferWritten < buffer.length ? bufferWritten : buffer.length - 1;
     buffer[zeroIndex] = '\0';
+
+    static if(assertOnTruncation) assert(!truncated);
+
     return buffer[0..bufferWritten];
 }
 
 void printOut(T, Args...)(T fmt, Args args)
-if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
+if(isCharArray!T)
 {
     FileHandle file = stdOut;
 
@@ -596,14 +592,14 @@ if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
 }
 
 void printOut(T)(T msg)
-if(isArray!(typeof(msg)) && isSomeChar!(typeof(msg[0])))
+if(isCharArray!T)
 {
     FileHandle file = stdOut;
     printFile(file, msg);
 }
 
 void printErr(T, Args...)(T fmt, Args args)
-if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
+if(isCharArray!T)
 {
     FileHandle file = stdErr;
 
@@ -614,14 +610,14 @@ if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
 }
 
 void printErr(T)(T msg)
-if(isArray!(typeof(msg)) && isSomeChar!(typeof(msg[0])))
+if(isCharArray!T)
 {
     FileHandle file = stdErr;
     printFile(file, msg);
 }
 
 void printFile(T, Args...)(FileHandle file, T fmt, Args args)
-if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
+if(isCharArray!T)
 {
     enum outputPolicy = `printFile(file, fmt[fmtCopyToBufferIndex .. fmtCursor]);`;
     enum formatPolicy = `formatArg(args[i], formatSpec, file);`;
@@ -630,7 +626,7 @@ if(isArray!(typeof(fmt)) && isSomeChar!(typeof(fmt[0])))
 }
 
 void printFile(T)(FileHandle file, T msg)
-if(isArray!(typeof(msg)) && isSomeChar!(typeof(msg[0])))
+if(isCharArray!T)
 {
     static if(use_cstdio)
     {
